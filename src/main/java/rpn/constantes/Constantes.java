f752511ee0e39d269cc98c7500cfc0403d98c919
KALFAA <theo92320@hotmail.fr>
2019-06-26 12:35:53 +0200

    first commit

---
+++ src/main/java/rpn/constantes/Constantes.java
@@ -0,0 +1,17 @@
+package rpn.constantes;
+
+public class Constantes {
+    public static final String TYPE_MESSAGE = "message";
+    public final static String TYPE_EOT = "eot";
+    public static final String TYPE_RESULT = "result";
+    public static final String TYPE_OPERATOR = "operator";
+    public static final String TYPE_EXPRESSION = "expression";
+    public final static String TYPE_ADDITION = "+";
+    public final static String TYPE_SOUSTRACTION = "-";
+    public final static String TYPE_MULTIPLICATION = "*";
+    public final static String TYPE_DIVISION = "/";
+
+
+
+
+}
